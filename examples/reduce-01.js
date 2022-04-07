2da6af13404f1eb5b108d7b4064fae38c3eb333c
palloncino <antonio.guiotto.dev@gmail.com>
2022-04-07 22:00:09 +0200

    reduce

--- examples/reduce-01.js
+++ examples/reduce-01.js
@@ -1,20 +1,14 @@
-const totalNumbers = 
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  .reduce((prev, curr) => {
-    return prev + curr;
-  }, 0);
+const reduce = (array, callback, initialValue) => {
+  array.forEach(num => {
+    initialValue += callback(num);
+  });
+  return initialValue;
+};
 
-const onlyConsonants = 
-  "The brown fox"
-  .split('').reduce((prev, curr) => {
-    !['a', 'e', 'i', 'o', 'u'].includes(curr) ? prev += curr : null;
-    return prev;
-  }, '');
+const result = reduce([1, 2, 3], value => value * 2, 0);
+console.log(result);
 
-const oddNumbers = () => {
-  return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].filter(n => (n % 2) == 0 ? null : n);
-}
+console.log(
+  [1, 2, 3].reduce((prev, curr) => prev += curr * 2, 0)
+);
 
-console.log(totalNumbers);
-console.log(onlyConsonants);
-console.log(oddNumbers());
