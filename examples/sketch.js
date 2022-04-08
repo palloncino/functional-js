0fbaeea719aef5cc2603c702f8ac8b650b67b7f6
palloncino <antonio.guiotto.dev@gmail.com>
2022-04-08 16:15:48 +0200

    15

--- examples/sketch.js
+++ examples/sketch.js
@@ -183,6 +183,6 @@ const groupOfFuncs = {};
 groupOfFuncs.double = n => n * 2;
 groupOfFuncs.addTen = n => n + 10;
 groupOfFuncs.inverse = n => n * -1;
-console.log(highestFunc(groupOfFuncs, 5)); // should log: 'addTen'
-console.log(highestFunc(groupOfFuncs, 11)); // should log: 'double'
-console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'
+// console.log(highestFunc(groupOfFuncs, 5)); // should log: 'addTen'
+// console.log(highestFunc(groupOfFuncs, 11)); // should log: 'double'
+// console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'
