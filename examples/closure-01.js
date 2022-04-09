8abc279b9921768a152796282d960c88dbc0fba2
palloncino <antonio.guiotto.dev@gmail.com>
2022-04-09 15:30:10 +0200

    10

--- examples/closure-01.js
+++ examples/closure-01.js
@@ -54,7 +54,7 @@ const jasCounter = outer();
 // jasCounter(); // counter, 1
 // willCounter(); // counter, 4
 
-// <---- CHALLENGE 4 ---->
+// <---- ✅ CHALLENGE 4 ---->
 // Now we are going to create a function addByX that returns a function that will add an input by x.
 
 const addByX = (x) => {
@@ -67,13 +67,13 @@ const addByTwo = addByX(2);
 
 // console.log(addByTwo(1)); // 3
 
-// <---- CHALLENGE 5 ---->
+// <---- ✅ CHALLENGE 5 ---->
 // Write a function once that accepts a callback as input and returns a function.
 // When the returned function is called the first time, it should call the callback and return that output.
 // If it is called any additional times, instead of calling the callback again it will simply return the output value from the first time it was called.
 const once = (func) => {
   let primeCall = undefined;
-  return function(x) {
+  return function (x) {
     if (!primeCall) {
       primeCall = func(x);
     }
@@ -88,12 +88,22 @@ const onceFunc = once(addByTwo);
 // console.log(onceFunc(10));  //should log 6
 // console.log(onceFunc(9001));  //should log 6
 
-// <---- CHALLENGE 6 ---->
+// <---- ✅ CHALLENGE 6 ---->
 // Write a function after that takes the number of times the callback needs to be called before being executed
 // as the first parameter and the callback as the second parameter.
-const after = (count, func) => {};
+const after = (count, func) => {
+  let counter = 0;
+  return function () {
+    ++counter;
+    if (counter === count) {
+      return func();
+    } else {
+      return console.log('run again ..');
+    }
+  };
+};
 
-const called = () => console.log('hello');
+const called = () => console.log('success');
 const afterCalled = after(3, called);
 
 // afterCalled(); // -> nothing is printed
@@ -101,26 +111,66 @@ const afterCalled = after(3, called);
 // afterCalled(); // -> 'hello' is printed
 
 // <---- CHALLENGE 7 ---->
-// Write a function delay that accepts a callback as the first parameter and the wait in milliseconds before allowing the callback to be invoked as the second parameter. Any additional arguments after wait are provided to func when it is invoked. HINT: research setTimeout();
-const delay = (func, wait) => {};
+// Write a function delay that accepts
+// a callback as the first parameter and
+// the wait in milliseconds before allowing the callback to be invoked as the second parameter.
+// Any additional arguments after wait are provided to func when it is invoked. HINT: research setTimeout();
+const delay = (func, wait) => {
+  return setTimeout(func, wait);
+};
 
-// <---- CHALLENGE 8 ---->
-// Create a function russianRoulette that accepts a number (let us call it n), and returns a function. The returned function will take no arguments, and will return the string 'click' the first n - 1 number of times it is invoked. On the very next invocation (the nth invocation), the returned function will return the string 'bang'. On every invocation after that, the returned function returns the string 'reload to play again'.
-const russianRoulette = (num) => {};
+// delay(() => console.log('go!'), 1500);
+
+// <---- ✅ CHALLENGE 8 ---->
+// Create a function russianRoulette that accepts a number (let us call it n), and returns a function.
+// The returned function will take no arguments, and will return the string 'click' the first n - 1 number of times it is invoked.
+// On the very next invocation (the nth invocation), the returned function will return the string 'bang'.
+// On every invocation after that, the returned function returns the string 'reload to play again'.
+const russianRoulette = (num) => {
+  let counter = 0;
+  return function () {
+    ++counter;
+    if (counter === num) {
+      return 'bang!';
+    } else if (counter > num) {
+      return 'reload to play again';
+    } else {
+      return 'click';
+    }
+  };
+};
 
 // /*** Uncomment these to check your work! ***/
-// const play = russianRoulette(3);
+const play = russianRoulette(3);
 // console.log(play()); // should log: 'click'
 // console.log(play()); // should log: 'click'
 // console.log(play()); // should log: 'bang'
 // console.log(play()); // should log: 'reload to play again'
 // console.log(play()); // should log: 'reload to play again'
 
-// <---- CHALLENGE 9 ---->
-const average = () => {};
+// <---- ✅ CHALLENGE 9 ---->
+// Create a function average that accepts no arguments,
+// and returns a function (that will accept either a number as its lone argument, or no arguments at all).
+// When the returned function is invoked with a number, the output should be average of all the numbers
+// have ever been passed into that function (duplicate numbers count just like any other number).
+// When the returned function is invoked with no arguments, the current average is outputted.
+// If the returned function is invoked with no arguments before any numbers are passed in, then it should return 0.
+const average = () => {
+  const nums = [];
+  let currentAverage = 0;
+  return function (x) {
+    if (!x) {
+      return currentAverage;
+    }
+    nums.push(x);
+    const total = nums.reduce((prev, curr) => prev + curr);
+    currentAverage = total / nums.length;
+    return currentAverage;
+  };
+};
 
 // /*** Uncomment these to check your work! ***/
-// const avgSoFar = average();
+const avgSoFar = average();
 // console.log(avgSoFar()); // should log: 0
 // console.log(avgSoFar(4)); // should log: 4
 // console.log(avgSoFar(8)); // should log: 6
@@ -129,18 +179,33 @@ const average = () => {};
 // console.log(avgSoFar()); // should log: 8
 
 // <---- CHALLENGE 10 ---->
-const makeFuncTester = (arrOfTests) => {};
+// Create a function makeFuncTester that accepts an array (of two-element sub-arrays),
+// and returns a function (that will accept a callback).
+// The returned function should return true if the first elements (of each sub-array)
+// being passed into the callback all yield the corresponding second elements (of the same sub-array).
+// Otherwise, the returned function should return false.
+const makeFuncTester = (arrOfTests) => {
+  return function(cb) {
+    let result = true;
+    arrOfTests.forEach((subArr) => {
+      if (cb(subArr[0]) !== subArr[1]) {
+        result = false;
+      }
+    });
+    return result;
+  };
+};
 
 // /*** Uncomment these to check your work! ***/
-// const capLastTestCases = [];
-// capLastTestCases.push(['hello', 'hellO']);
-// capLastTestCases.push(['goodbye', 'goodbyE']);
-// capLastTestCases.push(['howdy', 'howdY']);
-// const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
-// const capLastAttempt1 = str => str.toUpperCase();
-// const capLastAttempt2 = str => str.slice(0, -1) + str.slice(-1).toUpperCase();
-// console.log(shouldCapitalizeLast(capLastAttempt1)); // should log: false
-// console.log(shouldCapitalizeLast(capLastAttempt2)); // should log: true
+const capLastTestCases = [];
+capLastTestCases.push(['hello', 'hellO']);
+capLastTestCases.push(['goodbye', 'goodbyE']);
+capLastTestCases.push(['howdy', 'howdY']);
+const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
+const capLastAttempt1 = str => str.toUpperCase();
+const capLastAttempt2 = str => str.slice(0, -1) + str.slice(-1).toUpperCase();
+console.log(shouldCapitalizeLast(capLastAttempt1)); // should log: false
+console.log(shouldCapitalizeLast(capLastAttempt2)); // should log: true
 
 // <---- CHALLENGE 11 ---->
 const makeHistory = (limit) => {};
